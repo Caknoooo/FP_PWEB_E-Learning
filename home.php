aa44f6d04a36046449594114ea9c3ff84bba8c71
jap <javakanaya@gmail.com>
2023-06-20 12:46:57 +0700

    feat: services section

--- home.php
+++ home.php
@@ -31,8 +31,39 @@
     </section>
     <!-- header section ends -->
 
+    <!-- services section starts -->
+    <section class="services">
+        <h1 class="heading-title"> our services </h1>
 
-    
+        <div class="box-container">
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>adventure</h3>
+            </div>
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>tour guide</h3>
+            </div>
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>trekking</h3>
+            </div>
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>camp fire</h3>
+            </div>
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>off road</h3>
+            </div>
+            <div class="box">
+                <img src="images/" alt="">
+                <h3>camping</h3>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section ends -->
 
     <!-- footer section start -->
     <section class="footer">
